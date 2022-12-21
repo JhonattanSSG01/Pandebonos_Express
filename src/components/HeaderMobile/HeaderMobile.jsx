9013c99bd0e5447100fead05fb9eae5f792115b6
Jhonattan Saavedra Gomez <jhonattanestivensaavedragomez@gmail.com>
2022-12-21 17:34:38 -0500

    Agregando icono del menu

--- src/components/HeaderMobile/HeaderMobile.jsx
+++ src/components/HeaderMobile/HeaderMobile.jsx
@@ -2,15 +2,20 @@ import React from 'react';
 import './headerMobile.css';
 import img from './assets/logo.jpeg';
 
+
 const HEADER_MOBILE = _ => {
-  return <>
-  <header>
-    <div className="headerContainer">
-      <img className='logo' src={img} alt="logo de la microempesa" />
-      <div className="iconNav"></div>
-    </div>
-  </header>
-  </>
+  return (
+    <>
+      <header>
+        <div className="headerContainer">
+          <img className="logo" src={img} alt="logo de la microempesa" />
+          <div className="iconNav">
+            <i className="ri-function-line icon"></i>
+          </div>
+        </div>
+      </header>
+    </>
+  );
 }
 
 export default HEADER_MOBILE;
