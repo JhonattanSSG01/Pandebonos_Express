7c457cbadd96e76933a0398806976464051a1638
Jhonattan Saavedra Gomez <jhonattanestivensaavedragomez@gmail.com>
2022-12-21 11:48:03 -0500

    Realizando el header en mobile

--- src/components/HeaderMobile/HeaderMobile.jsx
+++ src/components/HeaderMobile/HeaderMobile.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import './headerMobile.css';
-import img from '../../assets/welcome.jpg';
+import img from './assets/logo.jpeg';
 
 const HEADER_MOBILE = _ => {
   return <>
   <header>
     <div className="headerContainer">
+      <img className='logo' src={img} alt="logo de la microempesa" />
+      <div className="iconNav"></div>
     </div>
-      <img src={img} alt="" style={{width: '95vw', margin: '1rem 0'}} />
-      <h1 style={{fontFamily: 'var(--fontPrincipal)', color: 'var(--colorText)', fontSize: '20px', margin: '0px'}}>Pandebonos Express</h1>
+      {/* <h1 style={{fontFamily: 'var(--fontPrincipal)', color: 'var(--colorText)', fontSize: '20px', margin: '0px'}}>Pandebonos Express</h1> */}
   </header>
   </>
 }
 
-export default HEADER_MOBILE
+export default HEADER_MOBILE;
