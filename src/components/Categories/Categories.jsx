051ba1196984a0a09ba8e90bb42f503f865f4420
Jhonattan Saavedra Gomez <jhonattanestivensaavedragomez@gmail.com>
2022-12-22 22:54:38 -0500

    Agregando prueba de mini json para verificar el pintado de categorias

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -2,30 +2,43 @@ import React from "react";
 import "./categories.css";
 import Header from "../Header/Header";
 import Product1 from "./assets/categoria1.jpg";
-import Product2 from './assets/categoria2.jpg';
-import Product3 from './assets/categoria3.jpg';
+import Product2 from "./assets/categoria2.jpg";
+import Product3 from "./assets/categoria3.jpg";
 import { Link } from "react-router-dom";
 import FooterAll from "../Delivery/Delivery";
+
+const imagenes = [
+  {
+    id: 1,
+    image: Product1,
+    title: "Productos Elaborados"
+  },
+  {
+    id: 2,
+    image: Product2,
+    title: "Insumos Para La Elaboracion"
+  },
+  {
+    id: 3,
+    image: Product3,
+    title: "Combos Temporadas Especiales"
+  },
+];
 
 const Categories = (_) => {
   return (
     <>
       <Header />
-      <Link to="/">
-        <div className="card">
-          <img src={Product1} alt="" />
-        </div>
-      </Link>
-      <Link to="/">
-        <div className="card">
-          <img src={Product2} alt="" />
-        </div>
-      </Link>
-      <Link to="/">
-        <div className="card">
-          <img src={Product3} alt="" />
-        </div>
-      </Link>
+      {imagenes.map((img) => {
+        return (
+          <Link to="/" className="link">
+            <div className="card" key={img.id}>
+              <img src={img.image} alt="" />
+              <h2>{img.title}</h2>
+            </div>
+          </Link>
+        );
+      })}
       <FooterAll />
     </>
   );
